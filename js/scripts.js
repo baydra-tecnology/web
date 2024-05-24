efb67a8683b7b78646348bfd354bff48390308f1
baydra-tecnology <baydra43@gmail.com>
2024-05-24 17:11:47 -0500

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,56 @@
+window.addEventListener('DOMContentLoaded', event => {
+
+    // Navbar shrink function
+    var navbarShrink = function () {
+        const navbarCollapsible = document.body.querySelector('#mainNav');
+        if (!navbarCollapsible) {
+            return;
+        }
+        if (window.scrollY === 0) {
+            navbarCollapsible.classList.remove('navbar-shrink')
+        } else {
+            navbarCollapsible.classList.add('navbar-shrink')
+        }
+
+    };
+
+    navbarShrink();
+
+    document.addEventListener('scroll', navbarShrink);
+
+    const mainNav = document.body.querySelector('#mainNav');
+    if (mainNav) {
+        new bootstrap.ScrollSpy(document.body, {
+            target: '#mainNav',
+            rootMargin: '0px 0px -40%',
+        });
+    };
+
+    const navbarToggler = document.body.querySelector('.navbar-toggler');
+    const responsiveNavItems = [].slice.call(
+        document.querySelectorAll('#navbarResponsive .nav-link')
+    );
+    responsiveNavItems.map(function (responsiveNavItem) {
+        responsiveNavItem.addEventListener('click', () => {
+            if (window.getComputedStyle(navbarToggler).display !== 'none') {
+                navbarToggler.click();
+            }
+        });
+    });
+
+});
+
+const fechaActual = new Date();
+const añoActual = fechaActual.getFullYear();
+document.getElementById("año-actual").innerHTML = añoActual;
+
+function changeImage(imageSrc, op) {
+    if (op === '1')
+        document.getElementById('mainImage1').src = imageSrc;
+
+    if (op === '2')
+        document.getElementById('mainImage2').src = imageSrc;
+
+    if (op === '3')
+        document.getElementById('mainImage3').src = imageSrc;
+}
